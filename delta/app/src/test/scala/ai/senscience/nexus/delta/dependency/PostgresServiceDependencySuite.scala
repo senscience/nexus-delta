327b0c36cc4a5f01f2e6e2b0cde426262eb5941a
Simon Dumas <simon.dumas@senscience.ai>
2025-08-15 17:20:48 +0200

    Fix test

--- delta/app/src/test/scala/ai/senscience/nexus/delta/dependency/PostgresServiceDependencySuite.scala
+++ delta/app/src/test/scala/ai/senscience/nexus/delta/dependency/PostgresServiceDependencySuite.scala
@@ -3,8 +3,11 @@ package ai.senscience.nexus.delta.dependency
 import ai.senscience.nexus.delta.kernel.dependency.ComponentDescription.ServiceDescription
 import ai.senscience.nexus.delta.sourcing.postgres.Doobie
 import ai.senscience.nexus.testkit.mu.NexusSuite
+import munit.AnyFixture
 
 class PostgresServiceDependencySuite extends NexusSuite with Doobie.Fixture {
+
+  override def munitFixtures: Seq[AnyFixture[?]] = List(doobie)
 
   test("fetch its service name and version") {
     val xas = doobie()
