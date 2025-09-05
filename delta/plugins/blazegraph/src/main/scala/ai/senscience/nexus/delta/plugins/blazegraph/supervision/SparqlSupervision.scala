4c4d426293d915ac1e07a99b3ed2c73fb391c8eb
Simon <simon.dumas@protonmail.com>
2025-09-05 16:57:15 +0200

    Refactor sparql supervision (#114)
    * Refactor sparql supervision to make it faster
    
    ---------
    
    Co-authored-by: Simon Dumas <simon.dumas@senscience.ai>

--- delta/plugins/blazegraph/src/main/scala/ai/senscience/nexus/delta/plugins/blazegraph/supervision/SparqlSupervision.scala
+++ delta/plugins/blazegraph/src/main/scala/ai/senscience/nexus/delta/plugins/blazegraph/supervision/SparqlSupervision.scala
@@ -1,10 +1,10 @@
 package ai.senscience.nexus.delta.plugins.blazegraph.supervision
 
-import ai.senscience.nexus.delta.kernel.Logger
 import ai.senscience.nexus.delta.plugins.blazegraph.client.SparqlClient
 import ai.senscience.nexus.delta.plugins.blazegraph.supervision.SparqlSupervision.SparqlNamespaceTriples
 import ai.senscience.nexus.delta.sdk.views.ViewRef
 import cats.effect.IO
+import cats.kernel.Monoid
 import cats.syntax.all.*
 import io.circe.syntax.KeyOps
 import io.circe.{Encoder, Json, JsonObject}
@@ -17,8 +17,6 @@ trait SparqlSupervision {
 }
 
 object SparqlSupervision {
-
-  private val logger = Logger[SparqlSupervision]
 
   /**
     * Returns the number of triples
@@ -33,15 +31,21 @@ object SparqlSupervision {
       total: Long,
       assigned: Map[ViewRef, Long],
       unassigned: Map[String, Long]
-  ) {
-    def +(view: ViewRef, count: Long): SparqlNamespaceTriples     =
-      copy(total = total + count, assigned = assigned + (view -> count))
-    def +(namespace: String, count: Long): SparqlNamespaceTriples =
-      copy(total = total + count, unassigned = unassigned + (namespace -> count))
-  }
+  )
 
   object SparqlNamespaceTriples {
     val empty: SparqlNamespaceTriples = SparqlNamespaceTriples(0L, Map.empty, Map.empty)
+
+    implicit val sparqlNamespaceTriplesMonoid: Monoid[SparqlNamespaceTriples] = new Monoid[SparqlNamespaceTriples] {
+      override def empty: SparqlNamespaceTriples = SparqlNamespaceTriples.empty
+
+      override def combine(x: SparqlNamespaceTriples, y: SparqlNamespaceTriples): SparqlNamespaceTriples =
+        SparqlNamespaceTriples(
+          x.total + y.total,
+          x.assigned ++ y.assigned,
+          x.unassigned ++ y.unassigned
+        )
+    }
 
     implicit final val sparqlNamespacesEncoder: Encoder[SparqlNamespaceTriples] = Encoder.AsObject.instance { value =>
       val assigned = value.assigned.toVector.sortBy(_._1.toString).map { case (view, count) =>
@@ -63,21 +67,17 @@ object SparqlSupervision {
   def apply(client: SparqlClient, viewsByNamespace: ViewByNamespace): SparqlSupervision =
     new SparqlSupervision {
       override def get: IO[SparqlNamespaceTriples] = {
-        for {
-          namespaces       <- client.listNamespaces
-          viewsByNamespace <- viewsByNamespace.get
-          result           <- namespaces.foldLeftM(SparqlNamespaceTriples.empty) { case (acc, namespace) =>
-                                client.count(namespace) recoverWith { th =>
-                                  logger.error(th)(s"Error while getting triples for namespace $namespace").as(-1L)
-                                } map { count =>
-                                  viewsByNamespace.get(namespace) match {
-                                    case Some(view) => acc + (view, count)
-                                    case None       => acc + (namespace, count)
-                                  }
-                                }
-                              }
-        } yield result
+        IO.both(client.listNamespaces, viewsByNamespace.get)
+          .flatMap { case (namespaces, viewsByNamespace) =>
+            namespaces.parFoldMapA { namespace =>
+              client.count(namespace).map { count =>
+                viewsByNamespace.get(namespace) match {
+                  case Some(view) => SparqlNamespaceTriples(count, Map(view -> count), Map.empty)
+                  case None       => SparqlNamespaceTriples(count, Map.empty, Map(namespace -> count))
+                }
+              }
+            }
+          }
       }
     }
-
 }
