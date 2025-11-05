7030df04a18ffb773cfc99016823446ae0b36652
Simon Dumas <simon.dumas@senscience.ai>
2025-11-05 14:54:33 +0100

    Use native otel env variable

--- tests/docker/config/delta-startup.sh
+++ tests/docker/config/delta-startup.sh
@@ -7,5 +7,4 @@ curl -f --silent -H "Content-Type: application/json" $ELASTICSEARCH_INDEX_TEMPLA
 ln -sf /opt/docker/plugins/disabled/project-deletion.jar /opt/docker/plugins/project-deletion.jar &&
 /opt/docker/bin/delta-app -Xmx4G \
   -Dotel.service.name=nexus-delta \
-  -Dotel.exporter.otlp.protocol=http/protobuf \
-  -Dotel.exporter.otlp.endpoint=http://otel-collector-contrib:4318/
+  -Dotel.exporter.otlp.protocol=http/protobuf
