f0b7f17ac123091223e45158814f91edf763d4df
Simon <simon.dumas@protonmail.com>
2025-11-26 17:37:14 +0100

    Only allow a given amount of elems in memory with backpressure (#198)
    * Only allow a given amount of elems in memory with backpressure
    
    ---------
    
    Co-authored-by: Simon Dumas <simon.dumas@senscience.ai>

--- delta/sourcing-psql/src/main/scala/ai/senscience/nexus/delta/sourcing/stream/config/BackpressureConfig.scala
+++ delta/sourcing-psql/src/main/scala/ai/senscience/nexus/delta/sourcing/stream/config/BackpressureConfig.scala
@@ -3,7 +3,7 @@ package ai.senscience.nexus.delta.sourcing.stream.config
 import pureconfig.ConfigReader
 import pureconfig.generic.semiauto.deriveReader
 
-case class BackpressureConfig(enabled: Boolean, bound: Int)
+case class BackpressureConfig(enabled: Boolean, bound: Int, maxElems: Int)
 
 object BackpressureConfig {
   given ConfigReader[BackpressureConfig] = deriveReader[BackpressureConfig]
