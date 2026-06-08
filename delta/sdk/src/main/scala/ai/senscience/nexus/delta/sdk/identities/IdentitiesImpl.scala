05aec7507344905265bec0e5834e62ae9cbc784e
Simon <simon.dumas@protonmail.com>
2026-06-08 17:16:35 +0200

    Use a single cache for realm resolution (#307)
    Co-authored-by: Simon Dumas <simon.dumas@senscience.ai>

--- delta/sdk/src/main/scala/ai/senscience/nexus/delta/sdk/identities/IdentitiesImpl.scala
+++ delta/sdk/src/main/scala/ai/senscience/nexus/delta/sdk/identities/IdentitiesImpl.scala
@@ -6,44 +6,39 @@ import ai.senscience.nexus.delta.kernel.jwt.{AuthToken, ParsedToken}
 import ai.senscience.nexus.delta.sdk.identities.IdentitiesImpl.logger
 import ai.senscience.nexus.delta.sdk.identities.model.Caller
 import ai.senscience.nexus.delta.sdk.realms.Realms
-import ai.senscience.nexus.delta.sdk.realms.model.Realm
 import ai.senscience.nexus.delta.sdk.syntax.*
 import ai.senscience.nexus.delta.sourcing.model.Identity.*
 import cats.data.OptionT
 import cats.effect.IO
 import cats.syntax.all.*
-import com.nimbusds.jose.jwk.JWKSet
 import org.http4s.client.Client
 import org.typelevel.otel4s.trace.Tracer
 
 class IdentitiesImpl private[identities] (
-    fetchRealm: FetchRealmByIssuer,
-    fetchJwks: FetchJwks,
+    realmResolver: RealmResolver,
     fetchRemoteGroups: FetchRemoteGroups
 )(using Tracer[IO])
     extends Identities {
 
   override def exchange(token: AuthToken): IO[Caller] = {
-    def fetchGroups(parsedToken: ParsedToken, realm: Realm): IO[Set[Group]] = {
+    def fetchGroups(parsedToken: ParsedToken, realm: ResolvedRealm): IO[Set[Group]] = {
       parsedToken.groups match {
         case Some(groups) => IO.pure(groups)
         case None         => fetchRemoteGroups(realm.userInfoEndpoint, parsedToken)
       }
     }.map(_.map(Group(_, realm.label)))
 
-    def validateToken(parsedToken: ParsedToken, realm: Realm)(jwks: JWKSet): IO[Unit] =
-      IO.fromEither(parsedToken.validate(realm.acceptedAudiences, jwks))
+    def validate(parsedToken: ParsedToken, realm: ResolvedRealm): IO[Unit] =
+      IO.fromEither(parsedToken.validate(realm.acceptedAudiences, realm.jwks))
 
-    def validateWithRetry(parsedToken: ParsedToken, realm: Realm): IO[Unit] = {
-      val validate = validateToken(parsedToken, realm)
-      fetchJwks(realm).flatMap(validate).recoverWith { case _: InvalidAccessToken =>
-        fetchJwks.refresh(realm).flatMap(validate)
+    def validateWithRetry(parsedToken: ParsedToken, realm: ResolvedRealm): IO[Unit] =
+      validate(parsedToken, realm).recoverWith { case _: InvalidAccessToken =>
+        realmResolver.refresh(realm).flatMap(validate(parsedToken, _))
       }
-    }
 
     val result = for {
       parsedToken <- IO.fromEither(ParsedToken.fromToken(token))
-      activeRealm <- OptionT(fetchRealm(parsedToken.issuer)).getOrRaise(UnknownAccessTokenIssuer)
+      activeRealm <- OptionT(realmResolver(parsedToken.issuer)).getOrRaise(UnknownAccessTokenIssuer)
       _           <- validateWithRetry(parsedToken, activeRealm)
       groups      <- fetchGroups(parsedToken, activeRealm)
       roles        = parsedToken.roles.fold(Set.empty)(_.map { r => Role(r, activeRealm.label) })
@@ -72,11 +67,10 @@ object IdentitiesImpl {
     *   the cache configuration
     */
   def apply(realms: Realms, client: Client[IO], config: IdentitiesConfig)(using Tracer[IO]): IO[Identities] = {
-    val fetchRealmByIssuer = FetchRealmByIssuer(realms, config.cache)
-    val fetchJwks          = FetchJwks(client, config.cache)
-    val fetchRemoteGroups  = FetchRemoteGroups(config.fetchRemoteGroups, client, config.cache)
+    val realmResolver     = RealmResolver(realms, client, config.cache)
+    val fetchRemoteGroups = FetchRemoteGroups(config.fetchRemoteGroups, client, config.cache)
 
-    (fetchRealmByIssuer, fetchJwks, fetchRemoteGroups).mapN(new IdentitiesImpl(_, _, _))
+    (realmResolver, fetchRemoteGroups).mapN(new IdentitiesImpl(_, _))
   }
 
 }
